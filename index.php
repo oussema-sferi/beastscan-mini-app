651ed4dfa87c7fc45df7f356dfd42ae7d38b7efc
Oussema Sferi <oussema.sferi@gmail.com>
2025-05-11 19:38:13 +0100

    setting up twig package + twig template

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+$twig = new \Twig\Environment($loader);
+
+// Render template (no dynamic data yet)
+echo $twig->render('index.html.twig');
